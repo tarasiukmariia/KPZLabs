979277149331c941e5f65cce4adfe9d2a5b25533
Mariia <vt221_tmo@student.ztu.edu.ua>
2024-05-30 21:54:26 +0300

    Початок лабораторної 5

---
+++ KPZLab5/Program/Program/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public abstract class LightNode
+{
+    public abstract string OuterHTML { get; }
+    public abstract string InnerHTML { get; }
+}
+
+public class LightTextNode : LightNode
+{
+    public string Text { get; set; }
+
+    public LightTextNode(string text)
+    {
+        Text = text;
+    }
+
+    public override string OuterHTML => Text;
+    public override string InnerHTML => Text;
+}
+
+public enum DisplayType
+{
+    Block,
+    Inline
+}
+
+public enum ClosingType
+{
+    SelfClosing,
+    WithClosingTag
+}
+
+public class LightElementNode : LightNode
+{
+    public string TagName { get; set; }
+    public DisplayType Display { get; set; }
+    public ClosingType Closing { get; set; }
+    public List<string> CssClasses { get; set; }
+    public List<LightNode> Children { get; set; }
+
+    public LightElementNode(string tagName, DisplayType display, ClosingType closing)
+    {
+        TagName = tagName;
+        Display = display;
+        Closing = closing;
+        CssClasses = new List<string>();
+        Children = new List<LightNode>();
+    }
+
+    public override string OuterHTML
+    {
+        get
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"<{TagName}");
+
+            if (CssClasses.Count > 0)
+            {
+                sb.Append($" class=\"{string.Join(" ", CssClasses)}\"");
+            }
+
+            if (Closing == ClosingType.SelfClosing)
+            {
+                sb.Append(" />");
+            }
+            else
+            {
+                sb.Append(">");
+                sb.Append(InnerHTML);
+                sb.Append($"</{TagName}>");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public override string InnerHTML
+    {
+        get
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var child in Children)
+            {
+                sb.Append(child.OuterHTML);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public void AddChild(LightNode child)
+    {
+        Children.Add(child);
+    }
+}
